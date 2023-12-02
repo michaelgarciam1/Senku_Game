541df4db627346ca14fc36fc9ed0b5a011edbd50
michaelgarciam1 <mgarciamoreira@cifpfbmoll.eu>
2023-12-02 18:40:25 +0100

    toast

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.senkugame"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.senkugame"
@@ -36,6 +36,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/senkugame/MainActivity.java
+++ app/src/main/java/com/example/senkugame/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.senkugame;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import androidx.gridlayout.widget.GridLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtén una referencia al layout principal
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        // Asigna un OnTouchListener al layout principal
+        gridLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                // Obtiene la posición de la pulsación
+                float x = event.getX();
+                float y = event.getY();
+
+                // Obtiene el ID del recurso XML
+//                int resourceId = getResourceIdFromTouch(x, y);
+                int column = getColumnFromTouch(x, gridLayout);
+                int row = getRowFromTouch(y, gridLayout);
+                // Muestra un Toast con la posición y el ID del recurso XML
+                showToast("Posición: (" + row + ", " + column + ") - ID: ");
+                return true;
+            }
+        });
     }
+    private int getColumnFromTouch(float x, GridLayout gridLayout) {
+        float columnWidth = gridLayout.getWidth() / gridLayout.getColumnCount();
+        return (int) (x / columnWidth);
+    }
+
+    // Método para obtener la fila tocada en el GridLayout
+    private int getRowFromTouch(float y, GridLayout gridLayout) {
+        float rowHeight = gridLayout.getHeight() / gridLayout.getRowCount();
+        return (int) (y / rowHeight);
+    }
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
